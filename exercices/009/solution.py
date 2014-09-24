403f58ce23c027fe44c6825774c3a06915afd117
augustinkrug <a.krug@ackr.net>
2014-09-24 15:54:58 +0200

    solut 009

--- exercices/009/solution.py
+++ exercices/009/solution.py
@@ -5,7 +5,7 @@ Created on Wed Sep 24 15:08:39 2014
 @author: augustin
 """
 
-phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
+text = """Turmoil has engulfed the Galactic Republic. The\
  taxation of trade routes to outlying star systems is in\
  dispute. Hoping to resolve the matter with a blockade of deadly\
  battleships, the greedy Trade Federation has stopped all shipping to\
@@ -13,16 +13,23 @@ phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
  endlessly debates this alarming chain of events, the Supreme\
  Chancellor has secretly dispatched two Jedi Knights, the guardians of\
  peace and justice in the galaxy, to settle the conflict"""
+
+"""
+Méthode brutus
+
 nb = 0
 mot = 0
-for i in range(len(phantom_menace)):
-    if phantom_menace[i] != " " and mot == 0:
+for i in range(len(text)):
+    if text[i] != " " and mot == 0:
         mot = 1
         nb = nb + 1
-    elif phantom_menace[i] != " " and mot == 1:
+    elif text[i] != " " and mot == 1:
         continue
-    elif phantom_menace[i] == " " and mot == 1:
+    elif text[i] == " " and mot == 1:
         mot = 0
-    elif phantom_menace[i] == " " and mot == 0:
+    elif text[i] == " " and mot == 0:
         continue
 print(nb)
+"""
+tt = text.split()
+print(len(tt))
