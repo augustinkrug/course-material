346bdab082442001409093666b14e951c701a019
augustinkrug <a.krug@ackr.net>
2014-09-24 16:49:45 +0200

    Correction 001

--- exercices/001/solution.py
+++ exercices/001/solution.py
@@ -4,7 +4,13 @@ Created on Wed Sep 24 09:44:27 2014
 
 @author: augustin
 """
+"""
+ERREUR : j'ai voulu faire une boucle alors qu'il suffisait d'afficher
+une chaine de caractère
+
 text = ''
 for i in range(ord('a'), ord('z')+1):
     text = text+chr(i)
 print(text)
+"""
+print("abcdefghijklnmopqrstuvwxyz")
