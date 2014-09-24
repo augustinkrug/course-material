c0918b2c182cb20683eaff50bc89d5211e2c793e
augustinkrug <a.krug@ackr.net>
2014-09-24 18:11:43 +0200

    Soluce 060

---
+++ exercices/060/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 18:07:05 2014
+
+@author: augustin
+"""
+
+for i in range(ord('a'), ord('z')+1):
+    for j in range(ord('a'), ord('z')+1):
+        print(chr(i) + chr(j))
