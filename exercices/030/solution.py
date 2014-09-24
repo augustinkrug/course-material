baa3aa93ec89a8196eebb730c7a3b2914330c89c
augustinkrug <a.krug@ackr.net>
2014-09-24 17:11:14 +0200

    Soluce 030

---
+++ exercices/030/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 16:55:18 2014
+
+@author: augustin
+"""
+even = 0
+for i in range(1, 100 + 1):
+    if divmod(i, 2)[1] == 0:
+        even = even + 1
+print(even)
